d0666d840c70582ea348dee20786502a3e22ec5a
Vladimir Blagojevic <dovlex@gmail.com>
2023-10-23 16:31:00 +0200

    Add GPTChatGenerator

--- haystack/preview/components/generators/__init__.py
+++ haystack/preview/components/generators/__init__.py
@@ -1,4 +1,3 @@
-from haystack.preview.components.generators.openai.gpt import GPTGenerator
 from haystack.preview.components.generators.hugging_face.hugging_face_local import HuggingFaceLocalGenerator
 
-__all__ = ["GPTGenerator", "HuggingFaceLocalGenerator"]
+__all__ = ["HuggingFaceLocalGenerator"]
---
+++ haystack/preview/components/generators/chat/__init__.py
@@ -0,0 +1,3 @@
+from haystack.preview.components.generators.chat.openai import GPTChatGenerator
+
+__all__ = ["GPTChatGenerator"]
